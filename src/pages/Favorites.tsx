1a718831cbd5c7a37047b4c8142626d4a74dd770
suphinhazainarr <suphinhassainar2004@gmail.com>
2025-04-28 13:30:35 +0530

    feat: enhance Favorites page to display user's favorite movies with improved UI

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -1,11 +1,36 @@
+import "../css/Favorites.css";
+import { useMovieContext } from "../contexts/MovieContext";
+import MovieCard from "../components/MovieCard";
 
-function Favorites() {
-  return (
-    <div className='favorites-empty'>
-        <h1>No favorite movies</h1>
-        <p>start adding favorites movies to the list</p>
-    </div>
-  )
+interface Movie {
+  imdbID: string;
+  Title: string;
+  Year: string;
+  Poster: string;
 }
 
-export default Favorites
+function Favorites() {
+  const { favorites }: { favorites: Movie[] } = useMovieContext();
+
+  if (favorites && favorites.length > 0) {
+    return (
+      <div className="favorites">
+        <h2>Your Favorites</h2>
+        <div className="movies-grid">
+          {favorites.map((movie) => (
+            <MovieCard movie={movie} key={movie.imdbID} />
+          ))}
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="favorites-empty">
+      <h2>No Favorite Movies Yet</h2>
+      <p>Start adding movies to your favorites and they will appear here!</p>
+    </div>
+  );
+}
+
+export default Favorites;
